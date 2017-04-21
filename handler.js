038857d1624cfec82f0c801ae034c52dc42d8403
Grayson Gilmore <gilmoreg@live.com>
2017-04-21 17:23:11 -0500

    getToken lambda

--- handler.js
+++ handler.js
@@ -1,16 +1,33 @@
-'use strict';
+const https = require('https');
 
-module.exports.hello = (event, context, callback) => {
-  const aniListEndpoint = 'https://anilist.co/api/';
-  const anilistAuthTokenPost = anilistEndPoint + 'auth/access_token?grant_type=client_credentials&client_id=solitethos-acaip&client_secret=gBg2dYIxJ3FOVuYPOGgHPGKHZ';
-	
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `${process.env.ANILIST_CLIENT_ID}`,
-      input: event,
-    }),
-  };
+const post = function(host, path) {
+  return new Promise((resolve, reject) => {
+    const request = https.request({ 
+      host,
+      path,
+      method: 'post'
+    }, (response) => {
+      const body = [];
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    });
+    request.on('error', (err) => reject(err));
+    request.end();
+    });
+};
 
-  callback(null, response);
+module.exports.getToken = (event, context, callback) => {
+  const host = 'anilist.co';
+  const path = `/api/auth/access_token?grant_type=client_credentials&client_id=${process.env.ANILIST_CLIENT_ID}&client_secret=${process.env.ANILIST_CLIENT_SECRET}`;
+  post(host, path)
+    .then((res) => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(res),
+      };
+      callback(null, response);
+    })
+    .catch((err) => {
+      callback(err, null);
+    });
 };
